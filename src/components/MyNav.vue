77255f5daca7b825e0b551793e661595c790ab67
donghaiyun <537085587@qq.com>
2021-03-10 21:56:06 +0800

    first commit

---
+++ src/components/MyNav.vue
@@ -0,0 +1,52 @@
+<template>
+  <nav>
+    <ul class="nav-tree">
+      <li class="nav-item">
+        <router-link to="/">
+          <a-icon type="home" />
+          <cite>主页</cite>
+          <i class="nav-more"></i>
+        </router-link>
+        <dl>
+          <dd></dd>
+        </dl>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+name: "MyNav"
+}
+</script>
+
+<style lang="scss">
+nav{
+  background-color: #20222A;
+  color: rgba(255,255,255,.7);
+}
+.nav-tree{
+  color: rgba(255,255,255,.7);
+  .nav-item{
+    a{
+      color: rgba(255,255,255,.9);
+    }
+    .nav-more{
+      display: inline-block;
+      border-style: solid dashed dashed;
+      border-color: #fff transparent transparent;
+      overflow: hidden;
+      cursor: pointer;
+      transition: all .2s;
+      -webkit-transition: all .2s;
+      border-width: 6px;
+      border-top-color: rgba(255,255,255,.7);
+    }
+  }
+  a:hover{
+    color: #fff;
+  }
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import antd from 'ant-design-vue'
+import MyNav from '@/components/MyNav'
 
+Vue.component('MyNav',MyNav);
 Vue.config.productionTip = false
-
+Vue.use(antd);
 new Vue({
   router,
   store,
